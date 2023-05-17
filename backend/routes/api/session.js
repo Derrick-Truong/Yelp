00d209eee63cd4a99b8bab543c386551fc9ab334
Derrick-Truong <dtruong169@gmail.com>
2023-05-17 01:33:06 -0700

    changes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,15 +6,25 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 const router = express.Router();
 // backend/routes/api/session.js
 // ...
 
 // Log in
 router.post(
-    '/',
+    '/', validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -46,7 +56,40 @@ router.post(
         return res.json({
             user: safeUser
         });
+
+
+
+    }
+
+
+);
+
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
     }
 );
+
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            const safeUser = {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+            };
+            return res.json({
+                user: safeUser
+            });
+        } else return res.json({ user: null });
+    }
+);
+
+
 
 module.exports = router;
