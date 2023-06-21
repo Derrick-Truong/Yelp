0e3bc7c2f324754ab0e37197c0746c871f54659e
Derrick <dtruong169@gmail.com>
2023-06-21 03:23:19 -0700

    help

--- frontend/src/components/Restaurant/TestRestaurant/index.js
+++ frontend/src/components/Restaurant/TestRestaurant/index.js
@@ -187,14 +187,13 @@ const TestRestaurant = () => {
                         <div className='restaurant-item-images-container-slider'>
 
                             <div className='restaurant-item-images-container-slider-track'>
-                                {restaurant?.RestaurantImages?.length ? restaurant?.RestaurantImages?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map(image => {
+                                {restaurant?.RestaurantImages?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map(image => {
                                     const url = 'https://icecreamfinder.s3.us-west-1.amazonaws.com/' + image?.url
 
                                     return (
                                         image && <img key={image.id} className="restaurant-item-restaurant-photos" src={url} alt='image' />
                                     )
-                                }) : <img src="https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg" alt="image" className="restaurant-item-restaurant-photos"/>}
-
+                                })}
                                 <div className='restaurant-item-info-container'>
                                     <div className='restaurant-info-inner-container'>
                                         <h1>{restaurant?.title}</h1>
