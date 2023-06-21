4d6e092a3e2df26d92b1903936a26fede7561367
Derrick <dtruong169@gmail.com>
2023-06-20 22:10:26 -0700

    please

--- frontend/src/components/Aws/Test1.js
+++ frontend/src/components/Aws/Test1.js
@@ -137,9 +137,10 @@ const Test1 = () => {
         // formData.append('newlisting', newListing)
 
         const newSpot = await dispatch(createNewRestaurant(form))
+        if ( newSpot && newSpot.id){
         await closeModal()
         await history.push(`/restaurants/${newSpot?.id}`)
-
+        }
         // const newSpot = await dispatch(createNewRestaurant(form)).then(closeModal).then(history.push(`/restaurants/${newSpot?.id}`))history.push(`/restaurants/${newSpot?.id}`);
 
 
