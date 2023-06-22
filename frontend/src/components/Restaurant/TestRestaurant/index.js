bc58fd912d30361546803db26493435e52bc9634
Derrick <dtruong169@gmail.com>
2023-06-22 03:13:16 -0700

    redo

--- frontend/src/components/Restaurant/TestRestaurant/index.js
+++ frontend/src/components/Restaurant/TestRestaurant/index.js
@@ -161,7 +161,7 @@ const TestRestaurant = () => {
         if (mapElement) {
             initMap();
         }
-    }, [dispatch, mapElement, restaurantId, reviews]);
+    }, [dispatch, mapElement, restaurantId, JSON.stringify(reviews)]);
     return (
         <>
             {/* <div className='loading-icon'>
